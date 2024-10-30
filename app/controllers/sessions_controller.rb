ebe5da71a2dad464af6b5ca3099ccf954219402d
Lucas Costa Gusmão <lucas.lcg20@gmail.com>
2024-10-30 15:49:46 -0300

    send message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(username: params[:username])
-    if user&.authenticate(params[:password])
+    user = User.find_by(username: params[:user][:username])
+    if user && user.authenticate(params[:user][:password])
       session[:user_id] = user.id
       flash[:success] = "You have successfully logged in"
       redirect_to root_path
