26a5735112a9fcfc3b2b8dd153c353421d54d592
Lucas Costa Gusmão <lucas.lcg20@gmail.com>
2024-10-31 10:27:09 -0300

    add broadcast

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "jquery" # @3.7.1
+pin "@rails/actioncable", to: "actioncable.esm.js"
+pin_all_from "app/javascript/channels", under: "channels"
